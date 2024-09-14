29bcc4641b199cc2f47c1e190c5cc27c1869a8aa
arisdev <preciadojulian849@gmail.com>
2024-09-13 20:16:15 -0600

    icon change

--- app.py
+++ app.py
@@ -450,12 +450,11 @@ def select_themes_tab():
     )
 
 
-with gr.Blocks(theme= loadThemes.load_json() or "NoCrypt/miku", title="🎵 UVR5 UI 🎵") as app:
-    gr.Markdown("<h1> 🎵 UVR5 UI 🎵 </h1>")
+with gr.Blocks(theme=loadThemes.load_json() or "NoCrypt/miku", title=" UVR5 UI ", icon="https://github.com/aris-py/UVR5-UI-Modified/blob/main/assets/uvricon.png?raw=true") as app:
+    gr.Markdown("<h1> UVR5 UI </h1>")
     gr.Markdown("If you like UVR5 UI you can star my repo on [GitHub](https://github.com/Eddycrack864/UVR5-UI)")
     gr.Markdown("Try UVR5 UI on Hugging Face with A100 [here](https://huggingface.co/spaces/TheStinger/UVR5_UI)")
     with gr.Tabs():
-
         with gr.TabItem("BS/Mel Roformer"):
             with gr.Row():
                 roformer_model = gr.Dropdown(
